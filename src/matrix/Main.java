24d05414ad920a1c61bf6749cb86185e0bb6bbd9
dick <plizzz.healme@gmail.com>
2019-10-21 00:11:03 +0300

    clean up code

--- src/matrix/Main.java
+++ src/matrix/Main.java
@@ -1,6 +1,5 @@
 package matrix;
 
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,68 +7,149 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 class Main {
 
+    private static final String INPUT_FILE = "in.txt";
+    private static final String OUTPUT_FILE = "out.txt";
+
     public static void main(String[] args) {
 
-        List<String> lines;
+        List<String> lines = readFromFile();
+        int[][] matrix;
 
-        try (Scanner in = new Scanner(new FileReader("in.txt"))) {
-            lines = Files.readAllLines(Paths.get("in.txt")); //считываем файл в список строк
-        } catch (IOException e) {
-            lines = new ArrayList<>(); //если ошибка, создаем пустой список
-        }
+        if (onlyNumbers(lines)) {
+            matrix = listToMatrix(lines);
 
-        int[][] matrix = new int[lines.size()][];
+            if (isMatrix(matrix)) {
 
-        /*
-         * TODO сделать проверку на корректность введенных данных
-         */
+                List<Integer> sortedRows = getSortedColumns(matrix);
+                int sortedCounter = sortedRows.size();
 
+                String outputString = "Отсортировано столбцов: " + sortedCounter + ". Номера столбцов: ";
 
-        int i = 0;
-        /*
-         * Преобразуем все в двумерный массив чисел
-         */
-        for (String line : lines) {
-            matrix[i] = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
-            i++;
-        }
-
-        //TODO проверить, одинаковый ли длины все строки
-
-        int columns = lines.size();
-        int rows = matrix[0].length;
-        int sortedCount = 0;
-
-        for (int l = 0; l < rows; l++) {
-            boolean isSorted = true;
-
-            for (int m = 0; m < columns - 1; m++) {
-                if (matrix[m][l] > matrix[m + 1][l]) {
-                    isSorted = false;
-                    break;
+                for (int column : sortedRows) {
+                    outputString += column + 1 + " ";
                 }
-            }
 
-            if (isSorted) {
-                sortedCount++;
+                System.out.println(outputString);
+                writeToFile(outputString);
+            } else {
+                System.out.println("Файл содержит не матрицу, проверьте количество цифр в каждом ряду");
             }
+        } else {
+            System.out.println("Ошибка в матрице. Файл должен содержать только числа");
+        }
+    }
+
+    /**
+     * Считывает файл in.txt в список строк
+     *
+     * @return список строк либо пустой список в случае ошибки
+     */
+    private static List<String> readFromFile() {
+        try {
+            return Files.readAllLines(Paths.get(INPUT_FILE));
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    /**
+     * Преобразует список строк в двумерный массив чисел
+     *
+     * @param lines список строк
+     * @return матрица в виде двумерного массива
+     */
+    private static int[][] listToMatrix(List<String> lines) {
+        int[][] matrix = new int[lines.size()][];
+        int row = 0;
+
+        for (String line : lines) {
+            matrix[row] = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+            row++;
         }
 
-        System.out.println("Отсортировано столбцов: " + sortedCount);
+        return matrix;
+    }
 
-        try(FileWriter writer = new FileWriter("out.txt", false))
-        {
-            writer.write("Отсортировано столбцов: " + sortedCount);
+    /**
+     * Ищет отсортированные по возрастанию столбцы в матрице
+     *
+     * @param matrix матрица
+     * @return список с номерами отсортированных столбцов
+     */
+    private static List<Integer> getSortedColumns(int[][] matrix) {
+        int rowsNumber = matrix.length;
+        int columnsNumber = matrix[0].length;
+        List<Integer> sortedRows = new ArrayList<>();
+
+        for (int column = 0; column < columnsNumber; column++) {
+            boolean sorted = true;
+            int raw = 0;
+
+            while (sorted && raw < rowsNumber - 1) {
+                if (matrix[raw][column] > matrix[raw + 1][column]) {
+                    sorted = false;
+                }
+                raw++;
+            }
+
+            if (sorted) {
+                sortedRows.add(column);
+            }
+        }
+        return sortedRows;
+    }
+
+    /**
+     * записывает строку в файл out.txt
+     *
+     * @param outputString строка с результатом
+     */
+    private static void writeToFile(String outputString) {
+        try (FileWriter writer = new FileWriter(OUTPUT_FILE, false)) {
+            writer.write(outputString);
             writer.flush();
-        }
-        catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-
-
     }
+
+    /**
+     * проверяет является ли массив матрицей (одинаковый ли длины строки)
+     *
+     * @param matrix матрица
+     * @return true если являетя, иначе false
+     */
+    private static boolean isMatrix(int[][] matrix) {
+        int size = matrix[0].length;
+
+        for (int[] raw : matrix) {
+            if (raw.length != size) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Проверка введенных данных
+     *
+     * @param lines список
+     * @return true если все верно, иначе false
+     */
+    private static boolean onlyNumbers(List<String> lines) {
+        String rexEx = "([0-9+-]*\\)*\\(*\\s*)+";
+
+        for (String line : lines) {
+            if (!line.matches(rexEx)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
